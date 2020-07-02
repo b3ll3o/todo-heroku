fd0b811a1c5e28c5588e9a42cc6a48e68e9504f3
Leonardo Andreazzi <leonardoandreazzi@gmail.com>
2020-07-01 23:19:14 -0300

    melhorias no codigo na edição de um post

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,6 +5,18 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import * as PostActions from '../store/actions/PostActions';
 
+export const useRedirect = path => {
+
+  const history = useHistory();
+
+  const redirect = () => {
+    if(path !== '')
+      history.push(path);
+  }
+  
+  return redirect;
+}
+
 export const useGetPosts = () => {
 
   const dispatch = useDispatch();
@@ -48,7 +60,7 @@ export const useEditPost = (id, tituloEditado, conteudoEditado, fixadoEditado, r
   useGetPosts();
   
   const dispatch = useDispatch();
-  const history = useHistory();
+  const redirect = useRedirect(redirectPath);
 
   const isCarregado = posts => (
     posts.length > 0
@@ -64,23 +76,28 @@ export const useEditPost = (id, tituloEditado, conteudoEditado, fixadoEditado, r
       : novoValor
   );
 
+  const setObjetoJson = chaves => valores => (
+    Object.fromEntries(chaves.map((_, i) => [chaves[i], valores[i]]))
+  )
+
   const setTitulo = setDefault(titulo);
   const setConteudo = setDefault(conteudo);
   const setFixado = setDefault(fixado);
 
+  const setEditado = setObjetoJson(['id', 'titulo', 'conteudo', 'fixado']);
 
   const editPost = e => {
     e.preventDefault(); 
 
-    dispatch(PostActions.edit({ 
-      id, 
-      titulo: setTitulo(tituloEditado),
-      conteudo: setConteudo(conteudoEditado), 
-      fixado: setFixado(fixadoEditado)
-    }));
+    dispatch(PostActions.edit(
+      setEditado([
+        id, 
+        setTitulo(tituloEditado), 
+        setConteudo(conteudoEditado), 
+        setFixado(fixadoEditado)])
+    ));
 
-    if(redirectPath !== '')
-      history.push(redirectPath);
+    redirect();
   }
 
   return [titulo, conteudo, fixado, editPost];
